5c23dff93d0846e972c34477da65dd0099dffb37
Mahmoud H Hamdy <lazy.coder.dev.exe@gmail.com>
2026-06-09 04:13:03 +0300

    Update Smallest Prime Factor.cpp

--- Smallest Prime Factor.cpp
+++ Smallest Prime Factor.cpp
@@ -18,9 +18,42 @@ void sieve() {
         }
     }
 }
+
 int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
+
     sieve();
+
+    // Check if number is prime
+    auto chk = [&](int n) {
+        return n == spf[n];
+    };
+
+    /*
+    Check if number is semi-prime
+    Note: A semi-prime is the product of two prime numbers (they may be equal)
+
+    For example:
+    25 = 5 * 5
+    77 = 7 * 11
+    */
+    auto semi = [&](int n) {
+        int x = spf[n];
+        int y = n / x;
+        return (spf[x] == x && x > 1 && spf[y] == y && y > 1);
+        // x * y == n
+    };
+
+    // Finding the prime factorization of a number
+    auto fac = [&](int n) {
+        vector<int> r;
+        int x = n;
+        while (x != 1) {
+            r.push_back(spf[x]);
+            x /= spf[x];
+        }
+        return r;
+    };
 
     return 0;
 }
