01e901cf2db9dc1888cc5237917a3f6eb5ffff3a
Mahmoud H Hamdy <lazy.coder.dev.exe@gmail.com>
2026-06-09 04:00:49 +0300

    Update Smallest Prime Factor.cpp

--- Smallest Prime Factor.cpp
+++ Smallest Prime Factor.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> spf;
+constexpr int n = 1e5 + 5;
+int spf[n];
 
-inline void build_spf(int n) {
-    spf.assign(n + 1, 0);
-
-    for (int i = 0; i <= n; i++)
+void sieve() {
+    for (int i = 1; i <= 100000; ++i) {
         spf[i] = i;
-
-    for (int i = 2; i * i <= n; i++) {
-        if (spf[i] == i) { // i is prime
-            for (int j = i * i; j <= n; j += i) {
+    }
+    for (int i = 2; i * i <= 100000; ++i) {
+        if (spf[i] == i) {
+            for (int j = i * i; j <= 100000; j += i) {
                 if (spf[j] == j) {
                     spf[j] = i;
                 }
@@ -19,19 +18,9 @@ inline void build_spf(int n) {
         }
     }
 }
-
-inline void done() {
-    int n;  
-    scanf("%d", &n);
-    build_spf(n);
-    for(auto i : spf) {
-        printf("%d ", i);
-    }
-}
-
 int32_t main() {
-    // cin.tie(0)->sync_with_stdio(0);
+    cin.tie(0)->sync_with_stdio(0);
+    sieve();
 
-    done();
     return 0;
 }
